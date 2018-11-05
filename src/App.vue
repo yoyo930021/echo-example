fc6b7c808750140eacc131dab64a02cbdf90e7a8
yoyo930021 <yoyo930021@gmail.com>
2018-11-05 16:13:40 +0800

    Init

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,59 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <input type="text" v-model="host">
+    <button @click="connect">連線</button><br><br>
+    <label>監聽<input type="text" placeholder="channel name" v-model="channel">頻道</label><br>
+    <label>的<input type="text" placeholder="event name" v-model="event">事件</label>
+    <button @click="join">監聽</button>
+    <button @click="joinPrivate">監聽私有頻道</button>
+    <br>
+    <div>{{content}}</div>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import { Component, Vue } from 'vue-property-decorator'
+import Echo from 'laravel-echo'
+import io from 'socket.io-client'
 
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
-export default class App extends Vue {}
+@Component
+export default class App extends Vue {
+  echo!: Echo
+  host: string = 'localhost:6001'
+  channel: string = ''
+  event: string = ''
+  content: string = ''
+
+  beforeMouted () {
+    (<any>window).io = io
+  }
+
+  connect () {
+    try {
+      this.echo = new Echo({
+        broadcaster: 'socket.io',
+        host: this.host
+      })
+      this.content = '連線成功'
+    } catch (e) {
+      this.content = '連線失敗'+'\n'+e.toString()
+    }
+  }
+
+  join () {
+    this.echo!.channel(this.channel).listen(this.event, (e: any) => {
+      this.content += '\n'
+      this.channel += e.toString()
+    })
+  }
+
+  joinPrivate () {
+    this.echo!.private(this.channel).listen(this.event, (e: any) => {
+      this.content += '\n'
+      this.channel += e.toString()
+    })
+  }
+}
 </script>
 
 <style>
